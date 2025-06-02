97defce3291cda88e1bb020e0d06a52fed69a693
James O'Leary <jpohhhh@gmail.com>
2025-06-02 14:05:01 -0400

    chore: lint

--- lib/nutdart.dart
+++ lib/nutdart.dart
@@ -2,5 +2,8 @@
 /// becomes a set of no-op placeholders so that dependent applications can
 /// still compile.  On desktop platforms (macOS, Windows, Linux) the full
 /// FFI implementation is used.
+
+library;
+
 export 'src/nutdart_stub.dart' if (dart.library.ffi) 'src/nutdart_real.dart';
 export 'src/nutdart_model.dart';
